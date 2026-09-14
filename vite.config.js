0433f8cf511f7f78f1e20edae102a9b9efe5f3ad
NIEVDI <61657627+NIEVDI@users.noreply.github.com>
2026-09-14 08:21:54 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/profnavigator-web/',
-})
-
-build: {
+  build: {
   target: 'es2015',              // Chrome 51+, Safari 10+, Firefox 54+, Edge 15+
   cssTarget: 'chrome61',         // Современные CSS-функции с фоллбэками
   polyfillDynamicImport: true,   // Поддержка динамических импортов
 }
+
+})
+
